0307d7cc3fbc6ccf5c1dfd9848c5670bf61620be
Raagam Parmar <raagamparmar@gmail.com>
2025-08-25 13:53:53 +0530

    Practice Questions

--- pr5/programs/asms/practice1.s
+++ pr5/programs/asms/practice1.s
@@ -1,14 +1,81 @@
-.section .data
-	# your data section goes here
-	# remove these two line comments when done
+	.section .data
+	.align   2
 
-.section .text
-.globl main
+sum:
+	.word    0
+
+arr:
+	.space   400
+
+	.section .text
+	.globl   main
+
 main:
-	# your code here
-	# remove these two line comments when done
+# practice 1: x10 = 0x1234abcd
+	lui      x1, 0x1234b
+	addi     x1, x1, -1075
 
+# practive 2: x4 = max(a0, a1, a2)
+	li       a0, 10
+	li       a1, 20
+	li       a2, 30
+	jal      max3
 
-	# let your code flow in here to end execution
+# practice 3: sum = x1 + x2
+	li       x1, 10
+	li       x2, 21
+	add      t0, x1, x2
+	la       t1, sum
+	sw       t0, 0(t1)
+
+# practice 4: arr[100] with all entries 0xc001
+	jal      init_arr
+
 halt:
-	j halt
+	j        halt
+
+
+# max2 n1 n2
+max2:
+	blt      a0, a1, a1_greater         # if a0 < a1 then a1 is greater
+	j        return
+a1_greater:
+	mv       a0, a1                     # a0 = a1
+return:
+	ret
+
+
+# max3 n1 n2 n3
+max3:
+# store ra to stack
+	addi     sp, sp, -4
+	sw       ra, 0(sp)
+
+	jal      max2
+	mv       a1, a2
+	jal      max2
+
+# restore ra from stack
+	lw       ra, 0(sp)
+	addi     sp, sp, 4
+
+	ret
+
+
+# init_arr
+init_arr:
+	li       t0, 99                     # i = 100 - 1 (arr size - 1)
+loop:
+	bltz     t0, exit
+
+	slli     t1, t0, 4                  # t1 = 4i (word addressed)
+	la       t2, arr
+	add      t3, t2, t1                 # t3 = &arr[i]
+	li       t4, 0xc001
+	sw       t4, 0(t3)
+
+	addi     t0, t0, -1
+	j        loop
+
+exit:
+	ret
--- pr5/programs/asms/practice2.s
+++ pr5/programs/asms/practice2.s
@@ -1,14 +1,32 @@
-.section .data
-	# your data section goes here
-	# remove these two line comments when done
+	.section .data
 
-.section .text
-.globl main
+arr_x:
+	.space   8
+
+arr_y:
+	.byte    104, 101, 108, 108, 111, 0
+
+	.section .text
+	.globl   main
+
 main:
-	# your code here
-	# remove these two line comments when done
+# practice 5: strcpy(x, y) [copy string y into x]
+	la       a0, arr_x
+	la       a1, arr_y
+	jal      ra, strcpy
 
-
-	# let your code flow in here to end execution
 halt:
-	j halt
+	j        halt
+
+
+# strcpy dest src
+strcpy:
+	mv       t0, a0
+	mv       t1, a1
+while:
+	lbu      t2, 0(t1)
+	sb       t2, 0(t0)
+	addi     t0, t0, 1
+	addi     t1, t1, 1
+	bnez     t2, while
+	ret
