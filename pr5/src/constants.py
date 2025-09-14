3fc212586ca64da2baf55a9a8c052972d2e39ab6
Raagam Parmar <raagamparmar@gmail.com>
2025-09-15 02:28:07 +0530

    Added constants module to hold constants. modified main to read files and load it into RAM.

---
+++ pr5/src/constants.py
@@ -0,0 +1,5 @@
+"""Width of RISC-V registers (32 bits for RV32)"""
+XWIDTH = 32
+
+"""Base address for loading binary into memory"""
+BASE_ADDR = 0x80000000
--- pr5/src/main.py
+++ pr5/src/main.py
@@ -1,1 +1,15 @@
-# your code here
+import sys
+
+from ram import RAM
+from constants import XWIDTH, BASE_ADDR
+
+ram = RAM(XWIDTH, XWIDTH)
+
+file = sys.argv[1]
+
+with open(file, "rb") as f:
+    ram.load(f.read(), BASE_ADDR)
+
+
+# ram.print_words(0x80002000, 0x80002040)
+# ram.print_words(0x80000000, 0x80008020)
