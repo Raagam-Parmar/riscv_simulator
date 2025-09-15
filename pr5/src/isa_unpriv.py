aea4aa876e76525ed324fe74e2d4b27d03ef0670
Raagam Parmar <raagamparmar@gmail.com>
2025-09-15 22:07:03 +0530

    Added post init check to ensure the fields are set correctly

--- pr5/src/isa_unpriv.py
+++ pr5/src/isa_unpriv.py
@@ -19,6 +19,47 @@ from enum import Enum, auto
 # The blank fields are illegal instructions in RV32-IMA-ZICSR
 
 
+def verify_bit(bit: int):
+    """Assert `bit` is either `0` or `1`"""
+    assert 0 <= bit <= 1, f'Invalid bit {bit}'
+
+def verify_2b_align(imm: int):
+    """Assert `imm` is even"""
+    assert imm & 0b1 == 0, f'Immediate not 2-byte aligned {imm}'
+
+def verify_reg(reg: int):
+    """Assert `reg` is in range `[0, 31]`"""
+    assert 0 <= reg <= 31, f'Invalid register x{reg}'
+
+def verify_imm12(imm: int):
+    """Assert `imm` is in range `[-2048, 2047]`"""
+    assert -2048 <= imm <= 2047, f'12-bit immediate out of range {imm}'
+
+def verify_imm20(imm: int):
+    """Assert `imm` is in range `[-524288, 524287]`"""
+    assert -524288 <= imm <= 524287, f'20-bit immediate out of range {imm}'
+
+def verify_uimm4(imm: int):
+    """Assert `imm` is in range `[0, 15]`"""
+    assert 0 <= imm <= 15, f'4-bit unsigned immediate out of range {imm}'
+
+def verify_uimm5(imm: int):
+    """Assert `imm` is in range `[0, 31]`"""
+    assert 0 <= imm <= 31, f'5-bit unsigned immediate out of range {imm}'
+
+def verify_uimm6(imm: int):
+    """Assert `imm` is in range `[0, 63]`"""
+    assert 0 <= imm <= 63, f'6-bit unsigned immediate out of range {imm}'
+
+def verify_uimm12(imm: int):
+    """Assert `imm` is in range `[0, 4095]`"""
+    assert 0 <= imm <= 4095, f'12-bit unsigned immediate out of range {imm}'
+
+def verify_uimm20(imm: int):
+    """Assert `imm` is in range `[0, 1048575]`"""
+    assert 0 <= imm <= 1048575, f'20-bit unsigned immediate out of range {imm}'
+
+
 # Reg instruction
 # +-----------+-------+------+---------+------------+-----------+
 # | funct7    | rs2   | rs1  | funct3  |     rd     |  0110011  |
@@ -57,6 +98,11 @@ class Reg:
     rs1: int
     rs2: int
     
+    def __post_init__(self):
+        verify_reg(self.rd)
+        verify_reg(self.rs1)
+        verify_reg(self.rs2)
+    
     def __str__(self):
         # op    rd, rs1, rs2
         return f"{self.op} \tx{self.rd}, x{self.rs1}, x{self.rs2}"
@@ -79,7 +125,7 @@ class Imm_ops(Enum):
     SLTIU = auto()
     
     JALR  = auto()
-    
+        
     def __str__(self):
         return self.name.lower()
 
@@ -89,7 +135,14 @@ class Imm:
     op : Imm_ops
     rd : int
     rs1: int
-    imm: int # encoded as -2048 <= imm <= 2047  (2**11 = 2048)
+    imm: int
+    # encoded as -2048 <= imm <= 2047  (2**11 = 2048)
+    # actual same as encoded
+    
+    def __post_init__(self):
+        verify_reg(self.rd)
+        verify_reg(self.rs1)
+        verify_imm12(self.imm)
     
     def __str__(self):
         # op    rd, rs1, imm
@@ -117,7 +170,14 @@ class Load:
     op : Load_ops
     rd : int
     rs1: int
-    imm: int # encoded as -2048 <= imm <= 2047  (2**11 = 2048)
+    imm: int
+    # encoded as -2048 <= imm <= 2047  (2**11 = 2048)
+    # actual same as encoded
+    
+    def __post_init__(self):
+        verify_reg(self.rd)
+        verify_reg(self.rs1)
+        verify_imm12(self.imm)
     
     def __str__(self):
         # op    rd, imm(rs1)
@@ -143,7 +203,14 @@ class Store:
     op : Store_ops
     rs1: int
     rs2: int
-    imm: int # encoded as -2048 <= imm <= 2047   (2**11 = 2048)
+    imm: int
+    # encoded as -2048 <= imm <= 2047   (2**11 = 2048)
+    # actual same as encoded
+    
+    def __post_init__(self):
+        verify_reg(self.rs1)
+        verify_reg(self.rs2)
+        verify_imm12(self.imm)
     
     def __str__(self):
         # op    rs2, imm(rs1)
@@ -172,7 +239,15 @@ class Branch:
     op : Branch_ops
     rs1: int
     rs2: int
-    imm: int # encoded as -2048 <= imm <= 2047   (2**11 = 2048)
+    # encoded as -2048 <= imm <= 2047   (2**11 = 2048)
+    # actual  as -4096 <= imm <= 4095   (2**12 = 4096) (evens only)
+    imm: int
+    
+    def __post_init__(self):
+        verify_reg(self.rs1)
+        verify_reg(self.rs2)
+        verify_2b_align(self.imm)
+        verify_imm12(self.imm // 2)
     
     def __str__(self):
         # op    rs1, rs2, imm
@@ -196,7 +271,13 @@ class Upper_ops(Enum):
 class Upper:
     op : Upper_ops
     rd : int
-    imm: int # encoded as 0 <= imm <= 1048575   (2**20 = 1048576)
+    imm: int
+    # encoded as 0 <= imm <= 1048575   (2**20 = 1048576)
+    # actual same as encoded [TODO TEST]
+    
+    def __post_init__(self):
+        verify_reg(self.rd)
+        verify_uimm20(self.imm)
     
     def __str__(self):
         # op    rd, imm
@@ -219,14 +300,20 @@ class Jump_ops(Enum):
 class Jump:
     op : Jump_ops
     rd : int
-    imm: int # encoded as -524288 <= imm <= 524287   (2**19 = 524288)
+    imm: int
+    # encoded as -524288 <= imm <= 524287   (2**19 = 524288)
+    # actual  as -1048575 <= imm <= 1048576 (2**20 = 1048576) (evens only)
+    
+    def __post_init__(self):
+        verify_reg(self.rd)
+        verify_imm20(self.imm // 2)
+        verify_2b_align(self.imm)
     
     def __str__(self):
         # op    rd, imm
         return f"{self.op} \tx{self.rd}, {self.imm}"
 
 
-# TODO add str repr
 # Misc Mem instruction
 # +--------+---------+--------+-------+-------+------+-----------+
 # |   fm   |  pred   | succ   |  rs1  |  000  |  rd  |  0001111  |
@@ -249,6 +336,13 @@ class Misc_mem:
     succ: int
     pred: int
     fm  : int
+    
+    def __post_init__(self):
+        verify_reg(self.rd)
+        verify_reg(self.rs1)
+        verify_uimm4(self.succ)
+        verify_uimm4(self.pred)
+        verify_uimm4(self.fm)
     
     def __str__(self):
         if self.op != Misc_mem_ops.FENCE:
@@ -304,8 +398,14 @@ class Atomic:
     aq : int
     rl : int
     
+    def __post_init__(self):
+        verify_reg(self.rd)
+        verify_reg(self.rs1)
+        verify_reg(self.rs2)
+        verify_bit(self.aq)
+        verify_bit(self.rl)
+    
     def __str__(self):
-        # op(.aq)(.rl)  rd, rs2, (rs1)
         show = f"{self.op}"
         
         if self.aq and not self.rl:
@@ -343,7 +443,7 @@ class System_ops(Enum):
 @dataclass
 class System:
     op : System_ops
-    
+        
     def __str__(self):
         # op
         return f"{self.op}"
@@ -369,12 +469,17 @@ class Zicsr:
     rs1: int
     csr: int
     
+    def __post_init__(self):
+        verify_reg(self.rd)
+        verify_reg(self.rs1)
+        verify_uimm12(self.csr)
+    
     def __str__(self):
         # op    rd, csr, rs1
         return f"{self.op} \tx{self.rd}, 0x{self.csr:03x}, x{self.rs1}"
 
 
-# Zicsr instruction
+# Zicsr Immediate instruction
 # +--------------------------+-------+----------+----------+-----------+
 # |            csr           | uimm  |  funct3  |  rd      |  1110011  |
 # +--------------------------+-------+----------+----------+-----------+
@@ -391,8 +496,15 @@ class Zicsr_imm_ops(Enum):
 class Zicsr_Imm:
     op  : Zicsr_imm_ops
     rd  : int
-    uimm: int # encoded as 0 <= uimm <= 32   (2**5 = 32)
     csr : int
+    uimm: int
+    # encoded as 0 <= uimm <= 32   (2**5 = 32)
+    # actual same as encoded
+    
+    def __post_init__(self):
+        verify_reg(self.rd)
+        verify_uimm5(self.uimm)
+        verify_uimm12(self.csr)
     
     def __str__(self):
         # op    rd, csr, uimm
