c2ef242a96f03ad86d89d838d3b0bd984a309a3c
Raagam Parmar <raagamparmar@gmail.com>
2025-10-12 16:08:33 +0530

    Working on simulator and single_cycle_processor. Test 1 and 2 working so far.

--- pr5/src/core/single_cycle_processor.py
+++ pr5/src/core/single_cycle_processor.py
@@ -1,24 +1,30 @@
+from ram import RAM
 from .processor import Processor
+from logger import PR5Logger
+
 
 class SingleCycleProcessor(Processor):
-    def __init__(self, start, ram, logger):
+    def __init__(self, start: int, ram: RAM, logger: PR5Logger):
         super().__init__(start, ram, logger)
 
-    def run(self, num_insts):
+    def run(self, num_insts: int) -> None:
         """
         Run the processor in a single cycle for each instruction.
         """
+
         i_cnt = 0
-        while (i_cnt < num_insts):
-            instruction = self.fetch()
-            if instruction is None:
-                break
-            self.decode(instruction)
-            self.operand_fetch()
-            self.execute()
-            self.mem_access()
-            self.update_pc()
-            self.reg_write()
+        while i_cnt < num_insts:
+            l1 = self.fetch()
+
+            # if instruction is None:
+            #     break
+
+            l2 = self.decode(l1)
+            l3 = self.execute(l2)
+            l4 = self.mem_access(l3)
+            self.update_pc(l3)
+            self.reg_write(l4)
+            
             i_cnt += 1
 
         self.logr.info(f"Simulated {i_cnt} instructions")
--- pr5/src/simulate.py
+++ pr5/src/simulate.py
@@ -1,25 +1,41 @@
 #!/usr/bin/env python3
 import argparse
 import os
+import logging
+
 import sys
 import ram
 import loader
 import logger
-import statistics
-import core
+
+# import statistics
+import core.single_cycle_processor
+from utils.constants import BYTE_WIDTH, XWIDTH
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="pr5 Simulator")
-    parser.add_argument('--start', type=lambda x: int(x, 16), required=True,
-                        help='Start PC in hex (e.g. 0x80000000)')
-    parser.add_argument('r5ob_path', type=str,
-                        help='Path to the input r5ob file')
-    parser.add_argument('--num_insts', type=int, default=1000,
-                        help='Number of instructions to simulate (default: 1000)')
+
+    parser.add_argument(
+        "--start",
+        type=lambda x: int(x, 16),
+        required=True,
+        help="Start PC in hex (e.g. 0x80000000)",
+    )
+
+    parser.add_argument("r5ob_path", type=str, help="Path to the input r5ob file")
+
+    parser.add_argument(
+        "--num_insts",
+        type=int,
+        default=1000,
+        help="Number of instructions to simulate (default: 1000)",
+    )
+
     return parser.parse_args()
 
-def run_simulation():
 
+def run_simulation():
     loggr = logger.setup()
     args = parse_args()
 
@@ -27,15 +43,20 @@ def run_simulation():
         loggr.error(f"Error: Executable file '{args.r5ob_path}' does not exist.")
         sys.exit(1)
 
-    mem = ram.RAM(loggr)
-    loader.load(mem, args.r5ob_path)
-    processor = core.SingleCycleProcessor(args.start, mem, loggr)
+    mem = ram.RAM(BYTE_WIDTH, XWIDTH, loggr)
+    # mem.print_words(0x80000000, 0x800000e0)
+    # loader.load(mem, args.r5ob_path, args.start)
+    loader.load(mem, args.r5ob_path, 0x80000000)
+    processor = core.single_cycle_processor.SingleCycleProcessor(args.start, mem, loggr)
 
     loggr.info(f"Start address: {hex(args.start)}")
     loggr.info(f"Executable path: {args.r5ob_path}")
     loggr.info(f"Number of instructions: {args.num_insts}")
+    
+    loggr.setLevel(logging.DEBUG)
 
     processor.run(args.num_insts)
+
 
 if __name__ == "__main__":
     run_simulation()
