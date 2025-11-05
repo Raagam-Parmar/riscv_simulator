760f2d195022518a478937402186df7804517046
Raagam Parmar <raagamparmar@gmail.com>
2025-11-05 11:10:50 +0530

    Added back support for operations of UIntN with integers with proper typing.

--- pr5/src/utils/cint/uintN.py
+++ pr5/src/utils/cint/uintN.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from math import ceil
-from typing import Self
+from typing import Self, cast
 from src.utils.bits import unsigned_max, sign_extend
 
 
@@ -24,6 +24,10 @@ class UIntN:
         cls.HEX_DIGITS = ceil(cls.UINT_WIDTH / 4)
 
     def __init__(self, value: int):
+        if not hasattr(self, "UINT_WIDTH"):
+            raise TypeError(
+                "UIntN cannot be instantiated directly. Use a subclass with width=N."
+            )
         self.value = value % (self.UINT_MAX + 1)
 
     # ---------------------------- Signed/Unsigned views ------------------------- #
@@ -46,129 +50,278 @@ class UIntN:
     def __neg__(self) -> Self:
         return type(self)(-self.value)
 
-    def __add__(self, other: Self) -> Self:
-        return type(self)(self.value + other.value)
+    def __add__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            return type(self)(self.value + other.value)
 
-    def __iadd__(self, other: Self) -> Self:
-        self.value = (self.value + other.value) % (self.UINT_MAX + 1)
+        other = cast(int, other)
+        return type(self)(self.value + other)
+
+    def __iadd__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            self.value = (self.value + other.value) % (self.UINT_MAX + 1)
+            return self
+
+        other = cast(int, other)
+        self.value = (self.value + other) % (self.UINT_MAX + 1)
         return self
 
-    def __radd__(self, other: Self) -> Self:
-        return type(self)(other.value + self.value)
+    def __radd__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            return type(self)(other.value + self.value)
 
-    def __sub__(self, other: Self) -> Self:
-        return type(self)(self.value - other.value)
+        other = cast(int, other)
+        return type(self)(other + self.value)
 
-    def __isub__(self, other: Self) -> Self:
-        self.value = (self.value - other.value) % (self.UINT_MAX + 1)
+    def __sub__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            return type(self)(self.value - other.value)
+
+        other = cast(int, other)
+        return type(self)(self.value - other)
+
+    def __isub__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            self.value = (self.value - other.value) % (self.UINT_MAX + 1)
+            return self
+
+        other = cast(int, other)
+        self.value = (self.value - other) % (self.UINT_MAX + 1)
         return self
 
-    def __rsub__(self, other: Self) -> Self:
-        return type(self)(other.value - self.value)
+    def __rsub__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            return type(self)(other.value - self.value)
 
-    def __mul__(self, other: Self) -> Self:
-        return type(self)(self.value * other.value)
+        other = cast(int, other)
+        return type(self)(other - self.value)
 
-    def __imul__(self, other: Self) -> Self:
-        self.value = (self.value * other.value) % (self.UINT_MAX + 1)
+    def __mul__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            return type(self)(self.value * other.value)
+
+        other = cast(int, other)
+        return type(self)(self.value * other)
+
+    def __imul__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            self.value = (self.value * other.value) % (self.UINT_MAX + 1)
+            return self
+
+        other = cast(int, other)
+        self.value = (self.value * other) % (self.UINT_MAX + 1)
         return self
 
-    def __rmul__(self, other: Self) -> Self:
-        return type(self)(other.value * self.value)
+    def __rmul__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            return type(self)(other.value * self.value)
 
-    def __floordiv__(self, other: Self) -> Self:
-        return type(self)(self.value // other.value)
+        other = cast(int, other)
+        return type(self)(other * self.value)
 
-    def __ifloordiv__(self, other: Self) -> Self:
-        self.value = (self.value // other.value) % (self.UINT_MAX + 1)
+    def __floordiv__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            return type(self)(self.value // other.value)
+
+        other = cast(int, other)
+        return type(self)(self.value // other)
+
+    def __ifloordiv__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            self.value = (self.value // other.value) % (self.UINT_MAX + 1)
+            return self
+
+        other = cast(int, other)
+        self.value = (self.value // other) % (self.UINT_MAX + 1)
         return self
 
-    def __rfloordiv__(self, other: Self) -> Self:
-        return type(self)(other.value // self.value)
+    def __rfloordiv__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            return type(self)(other.value // self.value)
 
-    def __mod__(self, other: Self) -> Self:
-        return type(self)(self.value % other.value)
+        other = cast(int, other)
+        return type(self)(other // self.value)
 
-    def __imod__(self, other: Self) -> Self:
-        self.value = (self.value % other.value) % (self.UINT_MAX + 1)
+    def __mod__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            return type(self)(self.value % other.value)
+
+        other = cast(int, other)
+        return type(self)(self.value % other)
+
+    def __imod__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            self.value = (self.value % other.value) % (self.UINT_MAX + 1)
+            return self
+
+        other = cast(int, other)
+        self.value = (self.value % other) % (self.UINT_MAX + 1)
         return self
 
-    def __rmod__(self, other: Self) -> Self:
-        return type(self)(other.value % self.value)
+    def __rmod__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            return type(self)(other.value % self.value)
+
+        other = cast(int, other)
+        return type(self)(other % self.value)
 
     # -------------------------------- Bitwise ----------------------------------- #
 
     def __invert__(self) -> Self:
         return type(self)(self.value ^ self.UINT_MAX)
 
-    def __and__(self, other: Self) -> Self:
-        return type(self)(self.value & other.value)
+    def __and__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            return type(self)(self.value & other.value)
 
-    def __iand__(self, other: Self) -> Self:
-        self.value &= other.value
+        other = cast(int, other)
+        return type(self)(self.value & other)
+
+    def __iand__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            self.value &= other.value
+            return self
+
+        other = cast(int, other)
+        self.value &= other
         return self
 
-    def __rand__(self, other: Self) -> Self:
-        return type(self)(other.value & self.value)
+    def __rand__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            return type(self)(other.value & self.value)
 
-    def __or__(self, other: Self) -> Self:
-        return type(self)(self.value | other.value)
+        other = cast(int, other)
+        return type(self)(other & self.value)
 
-    def __ior__(self, other: Self) -> Self:
-        self.value |= other.value
+    def __or__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            return type(self)(self.value | other.value)
+
+        other = cast(int, other)
+        return type(self)(self.value | other)
+
+    def __ior__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            self.value |= other.value
+            return self
+
+        other = cast(int, other)
+        self.value |= other
         return self
 
-    def __ror__(self, other: Self) -> Self:
-        return type(self)(other.value | self.value)
+    def __ror__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            return type(self)(other.value | self.value)
 
-    def __xor__(self, other: Self) -> Self:
-        return type(self)(self.value ^ other.value)
+        other = cast(int, other)
+        return type(self)(other | self.value)
 
-    def __ixor__(self, other: Self) -> Self:
-        self.value ^= other.value
+    def __xor__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            return type(self)(self.value ^ other.value)
+
+        other = cast(int, other)
+        return type(self)(self.value ^ other)
+
+    def __ixor__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            self.value ^= other.value
+            return self
+
+        other = cast(int, other)
+        self.value ^= other
         return self
 
-    def __rxor__(self, other: Self) -> Self:
-        return type(self)(other.value ^ self.value)
+    def __rxor__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            return type(self)(other.value ^ self.value)
 
-    def __lshift__(self, other: Self) -> Self:
-        shift = other.value
+        other = cast(int, other)
+        return type(self)(other ^ self.value)
+
+    def __lshift__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            shift = other.value
+            shift %= self.UINT_WIDTH
+            return type(self)(self.value << shift)
+
+        other = cast(int, other)
+        shift = other
         shift %= self.UINT_WIDTH
         return type(self)(self.value << shift)
 
-    def __ilshift__(self, other: Self) -> Self:
-        shift = other.value
+    def __ilshift__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            shift = other.value
+            shift %= self.UINT_WIDTH
+            self.value = (self.value << shift) & self.UINT_MAX
+            return self
+
+        other = cast(int, other)
+        shift = other
         shift %= self.UINT_WIDTH
         self.value = (self.value << shift) & self.UINT_MAX
         return self
 
-    def __rlshift__(self, other: Self) -> Self:
-        shift = other.value
-        shift %= self.UINT_WIDTH
-        return type(self)(other.value << shift)
+    def __rlshift__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            shift = other.value
+            shift %= self.UINT_WIDTH
+            return type(self)(other.value << shift)
 
-    def __rshift__(self, other: Self) -> Self:
-        shift = other.value
+        other = cast(int, other)
+        shift = other
+        shift %= self.UINT_WIDTH
+        return type(self)(other << shift)
+
+    def __rshift__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            shift = other.value
+            shift %= self.UINT_WIDTH
+            return type(self)(self.value >> shift)
+
+        other = cast(int, other)
+        shift = other
         shift %= self.UINT_WIDTH
         return type(self)(self.value >> shift)
 
-    def __irshift__(self, other: Self) -> Self:
-        shift = other.value
+    def __irshift__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            shift = other.value
+            shift %= self.UINT_WIDTH
+            self.value >>= shift
+            return self
+
+        other = cast(int, other)
+        shift = other
         shift %= self.UINT_WIDTH
         self.value >>= shift
         return self
 
-    def __rrshift__(self, other: Self) -> Self:
-        shift = other.value
+    def __rrshift__(self, other: Self | int) -> Self:
+        if isinstance(other, type(self)):
+            shift = other.value
+            shift %= self.UINT_WIDTH
+            v = other.value
+            return type(self)((v % (self.UINT_MAX + 1)) >> shift)
+
+        other = cast(int, other)
+        shift = other
         shift %= self.UINT_WIDTH
-        v = other.value
+        v = other
         return type(self)((v % (self.UINT_MAX + 1)) >> shift)
 
-    def sra(self, other: Self) -> Self:
+    def sra(self, other: Self | int) -> Self:
         """
         Arithmetic right shift
         """
-        shift = other.value
+        if isinstance(other, type(self)):
+            shift = other.value
+            shift %= self.UINT_WIDTH
+            return type(self)(self.signed() >> shift)
+
+        other = cast(int, other)
+        shift = other
         shift %= self.UINT_WIDTH
         return type(self)(self.signed() >> shift)
 
