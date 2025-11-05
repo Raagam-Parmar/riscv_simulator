66e0324bc6ffd40876a927abb43bd28f28644b83
Raagam Parmar <raagamparmar@gmail.com>
2025-11-05 11:35:00 +0530

    Added documentation and more properties for ISA.

--- pr5/src/isa/properties.py
+++ pr5/src/isa/properties.py
@@ -4,11 +4,25 @@ from typing import Union, TypeGuard
 
 from src.isa.instructions import *
 
+
+class UnimplementedExecution(Exception):
+    def __init__(self, inst: Instruction):
+        self.message = f"Execution of instruction '{inst}' is not implemented."
+        super().__init__(self.message)
+
+
 Instruction_with_rs1 = Union[
     Reg_reg, Reg_imm, Load, Store, Branch, Misc_mem, Atomic, Zicsr_reg_reg, Jalr
 ]
+"""
+Instructions which have an `rs1` field
+"""
 
 Instruction_with_rs2 = Union[Reg_reg, Store, Branch, Atomic]
+"""
+Instructions which have an `rs2` field
+"""
+
 
 Instruction_with_rd = Union[
     Reg_reg,
@@ -21,23 +35,52 @@ Instruction_with_rd = Union[
     Zicsr_reg_reg,
     Zicsr_reg_imm,
 ]
+"""
+Instructions which have an `rd` field
+"""
+
 
 Instruction_with_imm = Union[
     Reg_imm, Load, Store, Branch, Upper_imm, Jal, Zicsr_reg_imm
 ]
+"""
+Instructions which have an `imm` field
+"""
+
+
+Exec_Unimpl = Union[Misc_mem | Atomic | Env | Zicsr_reg_imm | Zicsr_reg_reg]
+"""
+Instructions whose execution is unimplemented
+"""
+
+
+Inst_modifies_pc = Union[Branch, Jal, Jalr]
+"""
+Instructions which modify the control flow
+"""
 
 
 def has_rs1(inst: Instruction) -> TypeGuard[Instruction_with_rs1]:
+    """
+    Does the instruction have an `rs1` field?
+    """
     return isinstance(
-        inst, (Reg_reg, Reg_imm, Load, Store, Branch, Misc_mem, Atomic, Zicsr_reg_reg, Jalr)
+        inst,
+        (Reg_reg, Reg_imm, Load, Store, Branch, Misc_mem, Atomic, Zicsr_reg_reg, Jalr),
     )
 
 
 def has_rs2(inst: Instruction) -> TypeGuard[Instruction_with_rs2]:
+    """
+    Does the instruction have an `rs2` field?
+    """
     return isinstance(inst, (Reg_reg, Store, Branch, Atomic))
 
 
 def has_rd(inst: Instruction) -> TypeGuard[Instruction_with_rd]:
+    """
+    Does the instruction have an `rd` field?
+    """
     return isinstance(
         inst,
         (
@@ -55,10 +98,16 @@ def has_rd(inst: Instruction) -> TypeGuard[Instruction_with_rd]:
 
 
 def has_imm(inst: Instruction) -> TypeGuard[Instruction_with_imm]:
+    """
+    Does the instruction have an `imm` field?
+    """
     return isinstance(
         inst, (Reg_imm, Load, Store, Branch, Upper_imm, Jal, Zicsr_reg_imm)
     )
 
 
-def modifies_pc(inst: Instruction) -> bool:
+def modifies_pc(inst: Instruction) -> TypeGuard[Inst_modifies_pc]:
+    """
+    Does the instruction modify the control flow?
+    """
     return isinstance(inst, (Branch, Jal, Jalr))
