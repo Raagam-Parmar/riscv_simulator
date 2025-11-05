d9bc71bea9e2dce36b81e4dcb39185b95156b4b1
Raagam Parmar <raagamparmar@gmail.com>
2025-11-05 21:50:02 +0530

    Added more ISA properties.

--- pr5/src/isa/properties.py
+++ pr5/src/isa/properties.py
@@ -53,10 +53,31 @@ Exec_Unimpl = Union[Misc_mem | Atomic | Env | Zicsr_reg_imm | Zicsr_reg_reg]
 Instructions whose execution is unimplemented
 """
 
+Exec_Impl = Union[Reg_reg, Reg_imm, Jalr, Load, Store, Branch, Upper_imm, Jal]
+"""
+Instructions whose execution is implemented
+"""
+
 
 Inst_modifies_pc = Union[Branch, Jal, Jalr]
 """
 Instructions which modify the control flow
+"""
+
+Inst_not_modifies_pc = Union[
+    Reg_reg,
+    Reg_imm,
+    Load,
+    Store,
+    Upper_imm,
+    Misc_mem,
+    Atomic,
+    Env,
+    Zicsr_reg_imm,
+    Zicsr_reg_reg,
+]
+"""
+Instructions which do not modify the control flow
 """
 
 
@@ -64,50 +85,53 @@ def has_rs1(inst: Instruction) -> TypeGuard[Instruction_with_rs1]:
     """
     Does the instruction have an `rs1` field?
     """
-    return isinstance(
-        inst,
-        (Reg_reg, Reg_imm, Load, Store, Branch, Misc_mem, Atomic, Zicsr_reg_reg, Jalr),
-    )
+    return isinstance(inst, Instruction_with_rs1)
 
 
 def has_rs2(inst: Instruction) -> TypeGuard[Instruction_with_rs2]:
     """
     Does the instruction have an `rs2` field?
     """
-    return isinstance(inst, (Reg_reg, Store, Branch, Atomic))
+    return isinstance(inst, Instruction_with_rs2)
 
 
 def has_rd(inst: Instruction) -> TypeGuard[Instruction_with_rd]:
     """
     Does the instruction have an `rd` field?
     """
-    return isinstance(
-        inst,
-        (
-            Reg_reg,
-            Reg_imm,
-            Load,
-            Upper_imm,
-            Jal,
-            Misc_mem,
-            Atomic,
-            Zicsr_reg_reg,
-            Zicsr_reg_imm,
-        ),
-    )
+    return isinstance(inst, Instruction_with_rd)
 
 
 def has_imm(inst: Instruction) -> TypeGuard[Instruction_with_imm]:
     """
     Does the instruction have an `imm` field?
     """
-    return isinstance(
-        inst, (Reg_imm, Load, Store, Branch, Upper_imm, Jal, Zicsr_reg_imm)
-    )
+    return isinstance(inst, Instruction_with_imm)
 
 
 def modifies_pc(inst: Instruction) -> TypeGuard[Inst_modifies_pc]:
     """
     Does the instruction modify the control flow?
     """
-    return isinstance(inst, (Branch, Jal, Jalr))
+    return isinstance(inst, Inst_modifies_pc)
+
+
+def not_modifies_pc(inst: Instruction) -> TypeGuard[Inst_not_modifies_pc]:
+    """
+    Does the instruction not modify the control flow?
+    """
+    return isinstance(inst, Inst_not_modifies_pc)
+
+
+def is_unimplemented(inst: Instruction) -> TypeGuard[Exec_Unimpl]:
+    """
+    Is the instruction not implemented for execution?
+    """
+    return isinstance(inst, Exec_Unimpl)
+
+
+def is_implemented(inst: Instruction) -> TypeGuard[Exec_Impl]:
+    """
+    Is the instruction implemented for execution?
+    """
+    return isinstance(inst, Exec_Impl)
